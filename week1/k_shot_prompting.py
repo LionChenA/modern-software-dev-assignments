524e59583058e88795410c006c1d2db483a94551
Lion <2679648321@qq.com>
2025-12-30 19:49:51 +0800

    feat: define system prompt for k-shot prompting task

--- week1/k_shot_prompting.py
+++ week1/k_shot_prompting.py
@@ -7,7 +7,7 @@ load_dotenv()
 NUM_RUNS_TIMES = 5
 
 # TODO: Fill this in!
-YOUR_SYSTEM_PROMPT = ""
+YOUR_SYSTEM_PROMPT = "You are a helpful assistant that outputs only the result, no explanations."
 
 USER_PROMPT = """
 Reverse the order of letters in the following word. Only output the reversed word, no other text:
